c13b2b320f212bcd3caed77750164a61bad63164
jplindgren <jplindgrenbr@hotmail.com>
2015-06-10 21:22:08 -0300

    add autocomplete to tags

---
+++ KnowledgeBase/Controllers/TagController.cs
@@ -0,0 +1,28 @@
+﻿using KnowledgeBase.Data;
+using KnowledgeBase.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnowledgeBase.Controllers{
+    public class TagController : Controller{
+        KnowledgeRepository repository;
+        JsonDataSource jsonDataSource;
+        public TagController() {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"App_Data\knowledge.json");
+            jsonDataSource = new JsonDataSource(path);
+            repository = new KnowledgeRepository(jsonDataSource);
+        }
+        //
+        // GET: /Tag/
+        public JsonResult Index() {
+            IList<Knowledge> results = repository.Load();
+            var tags = results.Select(x => x.Tag.Name).ToList();
+            return Json(tags, JsonRequestBehavior.AllowGet);
+        }
+
+    } //class
+}
