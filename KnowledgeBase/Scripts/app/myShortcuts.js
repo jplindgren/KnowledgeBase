e406cf2c2faec4102e2503c93dfd3d6a391e8e98
João Paulo Pozo Lindgren <jplindgrenbr@hotmail.com>
2015-06-12 23:19:43 -0700

    add shortcuts

---
+++ KnowledgeBase/Scripts/app/myShortcuts.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    shortcut.add("Ctrl+A", 	function() { 
+        $('#addArticleButton').dropdown('toggle');
+        var focusControl = $('#name');
+        if (focusControl.is(':visible'))
+            focusControl.focus();
+
+    }, { 'type': 'keydown', 'propagate': false, 'target': document });
+
+    shortcut.add("Ctrl+S", function (){
+        $('#search').focus();
+    }, { 'type': 'keydown', 'disable_in_input:': true, 'target': document });
+});
